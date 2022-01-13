a911f28f5e9fddd0e828d4239d071ffb234136bb
hhk9292 <hhk9292@gmail.com>
2022-01-13 16:50:41 +0900

    study: Sleact 2-6 라우터 주소 설계(라우트 파라미터)

--- react/front/components/CreateChannelModal/index.tsx
+++ react/front/components/CreateChannelModal/index.tsx
@@ -1,25 +1,82 @@
 import React, { VFC, useCallback } from 'react';
+import { useParams } from 'react-router';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import useSwr from 'swr';
 
 import Modal from '@components/Modal';
 import { Button, Input, Label } from '@pages/SignUp/styles';
 import useInput from '@hooks/useInput';
+import { IChannel, IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
 
 interface Props {
   show: boolean;
   onCloseModal: () => void;
+  setShowCreateChannelModal: (flag: boolean) => void;
 }
 
-const CreateChannelModal: VFC<Props> = ({ show, onCloseModal }: Props) => {
-  const [newChannel, onChangeNewChannel] = useInput<string>('');
+interface Params {
+  workspace: string;
+  channel: string;
+}
 
-  const onCreateChannel = useCallback(() => {}, []);
+const CreateChannelModal: VFC<Props> = ({
+  show,
+  onCloseModal,
+  setShowCreateChannelModal,
+}: Props) => {
+  const [newChannel, onChangeNewChannel, setNewChannel] = useInput<string>('');
+  const { workspace, channel } = useParams<Params>();
+  const {
+    data: userData,
+    error,
+    mutate,
+  } = useSwr<IUser | false>('/api/users', fetcher, {
+    dedupingInterval: 2000,
+  });
+
+  const { data: channelData, mutate: mutateChannel } = useSwr<IChannel[]>(
+    userData ? `/api/workspaces/${workspace}/channels` : null,
+    fetcher,
+  );
+
+  const onCreateChannel = useCallback(
+    (e: React.FormEvent) => {
+      e.preventDefault();
+      axios
+        .post(
+          `/api/workspaces/${workspace}/channels`,
+          {
+            name: newChannel,
+          },
+          {
+            withCredentials: true,
+          },
+        )
+        .then(() => {
+          setShowCreateChannelModal(false);
+          mutateChannel();
+          setNewChannel('');
+        })
+        .catch((error) => {
+          console.dir(error);
+          toast.error(error.response?.data, { position: 'bottom-center' });
+        });
+    },
+    [newChannel],
+  );
 
   return (
     <Modal show={show} onCloseModal={onCloseModal}>
       <form onSubmit={onCreateChannel}>
         <Label id="channel-label">
           <span>채널</span>
-          <Input id="channel" value={newChannel} onChange={onChangeNewChannel}></Input>
+          <Input
+            id="channel"
+            value={newChannel}
+            onChange={onChangeNewChannel}
+          ></Input>
         </Label>
         <Button type="submit">생성하기</Button>
       </form>
--- react/front/layouts/Workspace/index.tsx
+++ react/front/layouts/Workspace/index.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useState, VFC } from 'react';
+import { useParams } from 'react-router';
 import { Redirect, Switch, Route, Link } from 'react-router-dom';
 import useSWR from 'swr';
 import axios from 'axios';
@@ -7,7 +8,7 @@ import loadable from '@loadable/component';
 import { toast } from 'react-toastify';
 
 import fetcher from '@utils/fetcher';
-import { IUser } from '@typings/db';
+import { IUser, IChannel } from '@typings/db';
 const Channel = loadable(() => import('@pages/Channel'));
 const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 import Menu from '@components/Menu';
@@ -31,11 +32,29 @@ import Modal from '@components/Modal';
 import CreateChannelModal from '@components/CreateChannelModal';
 import { Button, Input, Label } from '@pages/SignUp/styles';
 import useInput from '@hooks/useInput';
+import { channel } from 'diagnostics_channel';
+import { createVerify } from 'crypto';
+
+interface Params {
+  workspace: string;
+  channel: string;
+}
 
 const Workspace: VFC = () => {
-  const { data: userData, error, mutate } = useSWR<IUser | false>('/api/users', fetcher);
+  const { workspace } = useParams<Params>();
+  const {
+    data: userData,
+    error,
+    mutate,
+  } = useSWR<IUser | false>('/api/users', fetcher);
+  const { data: channelData } = useSWR<IChannel[]>(
+    userData ? `/api/workspaces/${workspace}/channels` : null,
+    fetcher,
+  );
+
   const [showUserMenu, setShowUserMenu] = useState(false);
-  const [showCreateWorkspaceModal, setShowCreateWorkspaceModal] = useState(false);
+  const [showCreateWorkspaceModal, setShowCreateWorkspaceModal] =
+    useState(false);
   const [showWorkspaceModal, setShowWorkspaceModal] = useState(false);
   const [showCreateChannelModal, setShowCreateChannelModal] = useState(false);
   const [newWorkspace, onChangeNewWorkspace, setNewWorkspace] = useInput('');
@@ -114,6 +133,8 @@ const Workspace: VFC = () => {
     setShowCreateChannelModal(true);
   }, []);
 
+  const onClickInviteWorkspace = useCallback(() => {}, []);
+
   if (!userData) {
     return <Redirect to="/login" />;
   }
@@ -128,9 +149,19 @@ const Workspace: VFC = () => {
               alt={userData.nickname}
             ></ProfileImg>
             {showUserMenu && (
-              <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onCloseUserProfile}>
+              <Menu
+                style={{ right: 0, top: 38 }}
+                show={showUserMenu}
+                onCloseModal={onCloseUserProfile}
+              >
                 <ProfileModal>
-                  <img src={gravatar.url(userData.nickname, { s: '36px', d: 'retro' })} alt={userData.nickname} />
+                  <img
+                    src={gravatar.url(userData.nickname, {
+                      s: '36px',
+                      d: 'retro',
+                    })}
+                    alt={userData.nickname}
+                  />
                   <div>
                     <span id="profile-name">{userData.nickname}</span>
                     <span id="profile-active">Active</span>
@@ -144,10 +175,12 @@ const Workspace: VFC = () => {
       </Header>
       <WorkspaceWrapper>
         <Workspaces>
-          {userData.Workspaces.map((ws) => {
+          {userData?.Workspaces?.map((ws) => {
             return (
               <Link key={ws.id} to={`/workspace/channel/일반`}>
-                <WorkspaceButton>{ws.name.slice(0, 1).toUpperCase()}</WorkspaceButton>
+                <WorkspaceButton>
+                  {ws.name.slice(0, 1).toUpperCase()}
+                </WorkspaceButton>
               </Link>
             );
           })}
@@ -159,19 +192,32 @@ const Workspace: VFC = () => {
             Sleact
           </WorkspaceName>
           <MenuScroll>
-            <Menu show={showWorkspaceModal} onCloseModal={toggleWorkspaceModal} style={{ top: 95, left: 80 }}>
+            <Menu
+              show={showWorkspaceModal}
+              onCloseModal={toggleWorkspaceModal}
+              style={{ top: 95, left: 80 }}
+            >
               <WorkspaceModal>
                 <h2>Sleact</h2>
                 <button onClick={onClickAddChannel}>채널 만들기</button>
                 <button onClick={onLogout}> 로그아웃 </button>
               </WorkspaceModal>
             </Menu>
+            {channelData?.map((v) => (
+              <div key={v.id}>{v.name}</div>
+            ))}
           </MenuScroll>
         </Channels>
         <Chats>
           <Switch>
-            <Route path="/workspace/channel" component={Channel} />
-            <Route path="/workspace/dm" component={DirectMessage} />
+            <Route
+              path="/workspace/:workspace/channel/:channel"
+              component={Channel}
+            />
+            <Route
+              path="/workspace/workspace/dm/:id"
+              component={DirectMessage}
+            />
           </Switch>
         </Chats>
       </WorkspaceWrapper>
@@ -191,7 +237,7 @@ const Workspace: VFC = () => {
       <CreateChannelModal
         show={showCreateChannelModal}
         onCloseModal={onCloseModal}
-        // setShowCreateChannelModal={setShowCreateChannelModal}
+        setShowCreateChannelModal={setShowCreateChannelModal}
       />
     </div>
   );
